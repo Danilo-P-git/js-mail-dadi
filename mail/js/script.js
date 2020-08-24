5105b31d16d5edb5ffbb18084c61e7179d6bddc9
Danilo Patanè <d.patane98@gmail.com>
2020-08-24 16:43:25 +0200

    Correction js

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,14 +1,20 @@
 var emailInserita = prompt("inserisci un email verificata");
 var emailDatabase = ["d.patane98@gmail.com", "dariomozzilla@gmail.com", "giuseppemozz@gmail.com"];
 
+var mailTrovata = false;
+
 for (var i = 0; i < emailDatabase.length; i++) {
   var thisMail = emailDatabase[i];
 
   if (thisMail == emailInserita)
     {
-        console.log("Accesso garantito");
+        mailTrovata = true;
     }
-  else if (thisMail != emailInserita ) {
-        console.log("Accesso negato")
-  }
+}
+
+if (mailTrovata = true) {
+  console.log("accesso consentito");
+}
+else if (mailTrovata = false) {
+  console.log("accesson negato");
 }
