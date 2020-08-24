551adc189913ec973d0eab31a488da014da31b1a
Danilo Patanè <d.patane98@gmail.com>
2020-08-24 15:50:21 +0200

    Correction mail

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,9 +1,11 @@
 var login = prompt("inserisci un email verificata");
 var ident = ["d.patane98@gmail.com", "dariomozzilla@gmail.com", "giuseppemozz@gmail.com"];
-if (login = ident.slice(0-2)) {
-  console.log("accesso garantito");
 
-}
-else {
-  console.log("errore");
+for (var i = 0; i < ident.length; i++) {
+  if (ident[i] = login) {
+    console.log("corretto")
+  }
+  else {
+    console.log("sbagliato");
+  }
 }
