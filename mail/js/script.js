c1b6fdd76e74e80c0614b01f0a8f99ccc3c11a69
Danilo Patanè <d.patane98@gmail.com>
2020-08-24 16:14:39 +0200

    Mail done

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,11 +1,14 @@
-var login = prompt("inserisci un email verificata");
-var ident = ["d.patane98@gmail.com", "dariomozzilla@gmail.com", "giuseppemozz@gmail.com"];
+var emailInserita = prompt("inserisci un email verificata");
+var emailDatabase = ["d.patane98@gmail.com", "dariomozzilla@gmail.com", "giuseppemozz@gmail.com"];
 
-for (var i = 0; i < ident.length; i++) {
-  if (ident[i] = login) {
-    console.log("corretto")
-  }
-  else {
-    console.log("sbagliato");
+for (var i = 0; i < emailDatabase.length; i++) {
+  var thisMail = emailDatabase[i];
+
+  if (thisMail == emailInserita)
+    {
+        console.log("Accesso garantito");
+    }
+  else if (thisMail != emailInserita ) {
+        console.log("Accesso negato")
   }
 }
