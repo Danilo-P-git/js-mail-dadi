9e3d9b2dea2ce3a1c4f8f905ef7df67d7f4ed2a8
Danilo Patanè <d.patane98@gmail.com>
2020-08-25 11:37:07 +0200

    Commenti

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,17 +1,18 @@
+// cheido l'email e inserisco il database dove devo verificare la sua esistenza
 var emailInserita = prompt("inserisci un email verificata");
 var emailDatabase = ["d.patane98@gmail.com", "dariomozzilla@gmail.com", "giuseppemozz@gmail.com"];
-
+// imposto una variabile boleana
 var mailTrovata = false;
-
+// inizio il ciclo
 for (var i = 0; i < emailDatabase.length; i++) {
   var thisMail = emailDatabase[i];
-
+// se è uguale la variabile boleana diventa true
   if (thisMail == emailInserita)
     {
         mailTrovata = true;
     }
 }
-
+// faccio un if se è vero stampo una cosa se falso un altra
 if (mailTrovata == true) {
   console.log("accesso consentito");
 }
